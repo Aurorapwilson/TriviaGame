e22f46aa888ae630361a3599edc319cba6f9bc90
Princess Wilson <aurorapwilson1@gmail.com>
2018-12-20 09:32:50 -0500

    before center

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//create an object that starts the timer
+let Timer
+//When we click the button two thing will happen
+object.addEventListener("click", myScript);
+//first when you click the start button you are taken to a second page that loads the form
+// At the same time when you click start the timer starts

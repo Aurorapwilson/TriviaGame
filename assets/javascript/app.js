aea3c9817833b3569d05f7a19c825e4cd3dcb079
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-17 15:12:09 -0500

    quest1 function not being called

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,4 @@
 
-
-//create an object that holds the time of 2 minutes to finish the game.
-
-//Then create an array for my questions
-// let questions = {
-//   Q1: {
-//     'The name of Jake\'s girlfriend is Lady Rainicorn': 't'
-//   },
-//   Q2: {
-//     'Marceline is a ghost': 'f'
-//   },
-//   Q3: {
-//     'Jake and Finn live in the world of Ooo': 't'
-//   },
-//   Q4: {
-//     'Jake has 18 children': 'f'
-//   },
-//   Q5: {
-//     'The voice of Finn the Human is Jason Bateman': 'f'
-//   },
-//   Q6: {
-//     'The Ice King kidnaps the princesses of Ooo from time to time': 't'
-//   },
-//   Q7: {
-//     'LSP is a Cloud Princess': 't'
-//   },
-//   Q8: {
-//     'Finn and Jake live in a cave atop a mountain': 'f'
-//   }
-// };
-
 //TODO Improve this and check about boolean answers
 let questions = [{
   question: 'The name of Jake\'s girlfriend is Lady Rainicorn',
@@ -111,7 +80,7 @@ let questions = [{
 let correctAnswers;
 let wrongAnswers;
 let intervalId;
-let time = 2;
+let time = 120;
 
 // Cache elements
 let questionContainer = $(".questionContainer");
@@ -125,29 +94,31 @@ $('.startBtn').click(function () {
 
 //remove the start button once clicked and show first answer
 $('button').css('display', "none");
+
 //then we want to run the first question function
 quest1();
 });
 
+
+
+
 //function to create new Div with the first question of our object
-
 function quest1() {
- //create new div for questions
- let newDiv = $('.questionContainer')
- $('.questionContainer').html(questions);
-
-function quest1() {
-//create new div for questions
-let newDiv = $('<div= "questions">')
-$(newDiv).html(questions);
-$('.jumbotron.text-center').append(newDiv);
 
 
 // when the first part of quest1 runs and our new question div appear we start our timer
-$('.btnContainer').append(newDiv);
+$('body').append(questionContainer);
+$('questionContainer').show(questions);
 intervalId = setInterval(timeCount, 1000);
 buildQuestions();
-}
+ //create new div for questions
+ questionContainer.html(questions);
+};
+
+
+
+
+
 
 //this function is our timer basically decreasing 1000milliseconds
 function timeCount() {
@@ -156,9 +127,13 @@ $("#show-number").html("<h2>" + time + "</h2>");
 //stops timer
 if (time === 0) {
   stopTimer();
-}
+};
 
-}
+};
+
+
+
+
 
 function stopTimer() {
 clearInterval(intervalId);
@@ -213,11 +188,4 @@ questions.forEach(
 
 // finally combine our output list into one string of HTML and put it on the page
 rowContainer.html(output);
-
-
-
-
-
-
-
-
+}
